6d7a2401941e27de68891a366e0e0067de825154
J-Naish <mjyrgd2332@i.softbank.jp>
2024-05-06 16:12:20 +0900

    create an image comp

---
+++ VisionaryTour/ListView/Helpers/RoundedSquareImage.swift
@@ -0,0 +1,25 @@
+//
+//  RoundedSquareImage.swift
+//  VisionaryTour
+//
+//  Created by JinNash on 2024/05/06.
+//
+
+import SwiftUI
+
+struct RoundedSquareImage: View {
+    var image: Image
+    
+    var body: some View {
+        image
+            .resizable()
+            .aspectRatio(contentMode: .fill)
+            .frame(width: 200, height: 200)
+            .clipShape(Rectangle())
+            .cornerRadius(32)
+    }
+}
+
+#Preview {
+    RoundedSquareImage(image: Image("dummy"))
+}
