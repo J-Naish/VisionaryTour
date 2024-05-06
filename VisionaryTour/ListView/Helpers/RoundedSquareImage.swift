8d86404a71ff1a8f0953a03b663af7f2b77657ea
J-Naish <mjyrgd2332@i.softbank.jp>
2024-05-07 00:41:49 +0900

    update rounded square image comp

--- VisionaryTour/ListView/Helpers/RoundedSquareImage.swift
+++ VisionaryTour/ListView/Helpers/RoundedSquareImage.swift
@@ -9,18 +9,44 @@ import SwiftUI
 
 struct RoundedSquareImage: View {
     var image: Image
-    var isWide: Bool
+    var size: Size
+    enum Size {
+        case small
+        case medium
+        case large
+        
+        var width: CGFloat {
+            switch self {
+            case .small:
+                return 200
+            case .medium:
+                return 420
+            case .large:
+                return 880
+            }
+        }
+        var height: CGFloat {
+            switch self {
+            case .small:
+                return 200
+            case .medium:
+                return 200
+            case .large:
+                return 200
+            }
+        }
+    }
     
     var body: some View {
         image
             .resizable()
             .aspectRatio(contentMode: .fill)
-            .frame(minWidth: 200, maxWidth: isWide ? 880 : 200, minHeight: 200, maxHeight: isWide ? 240 : 200, alignment: .center)
+            .frame(width: size.width, height: size.height)
             .clipShape(Rectangle())
             .cornerRadius(20)
     }
 }
 
 #Preview {
-    RoundedSquareImage(image: Image("dummy"), isWide: true)
+    RoundedSquareImage(image: Image("dummy"), size: .large)
 }
--- VisionaryTour/ListView/ListView.swift
+++ VisionaryTour/ListView/ListView.swift
@@ -17,7 +17,19 @@ struct ListView: View {
             }
             .navigationTitle("List")
         } detail: {
-            
+            VStack {
+                RoundedSquareImage(image: Image("dummy"), size: .large)
+                HStack(spacing: 32) {
+                    RoundedSquareImage(image: Image("dummy"), size: .medium)
+                    RoundedSquareImage(image: Image("dummy"), size: .medium)
+                }
+                HStack(spacing: 24) {
+                    RoundedSquareImage(image: Image("dummy"), size: .small)
+                    RoundedSquareImage(image: Image("dummy"), size: .small)
+                    RoundedSquareImage(image: Image("dummy"), size: .small)
+                    RoundedSquareImage(image: Image("dummy"), size: .small)
+                }
+            }
         }
         .searchable(text: $searchText)
     }
