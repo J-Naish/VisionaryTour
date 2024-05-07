d203a2565b95bcb51f46942f8fa90250ba9a88b3
J-Naish <mjyrgd2332@i.softbank.jp>
2024-05-08 02:43:01 +0900

    pack map control buttons as new comp

---
+++ VisionaryTour/Views/MapView/Helpers/MapControlButton.swift
@@ -0,0 +1,95 @@
+//
+//  MapControlButton.swift
+//  VisionaryTour
+//
+//  Created by JinNash on 2024/05/08.
+//
+
+import SwiftUI
+
+struct MapControlButton: View {
+    @Binding var isMapSelected: Bool
+    let viewModel: MapViewModel
+    
+    var body: some View {
+        ZStack {
+            
+            Capsule()
+                .fill(Color(red:1.0, green: 1.0, blue: 1.0, opacity: 0.45))
+                .frame(width: 280, height: 48)
+            
+            HStack {
+                Button(action: {
+                    withAnimation {
+                        viewModel.updateMapType("roadmap")
+                        isMapSelected = true
+                    }
+                }) {
+                    Text("")
+                        .frame(width: 64, height: 14)
+                        .padding()
+                        .background(
+                            Capsule()
+                                .fill(Color.clear)
+                        )
+                        .contentShape(Capsule())
+                }
+                
+                Button(action: {
+                    withAnimation {
+                        viewModel.updateMapType("satellite")
+                        isMapSelected = false
+                    }
+                }) {
+                    Text("")
+                        .frame(width: 64, height: 14)
+                        .padding()
+                        .background(
+                            Capsule()
+                                .fill(Color.clear)
+                        )
+                        .contentShape(Capsule())
+                }
+            }
+            
+            Capsule()
+                .fill(Color.clear)
+                .innerShadow(color: Color.black.opacity(0.2), radius: 2)
+                .frame(width: 280, height: 48)
+                .allowsHitTesting(false)
+            
+            CapsuleBackground(isMapSelected: $isMapSelected)
+                .allowsHitTesting(false)
+            
+            HStack {
+                Text("Map")
+                    .frame(width: 136, height: 14)
+                    .foregroundColor(isMapSelected ? Color.black : Color.white)
+                    .allowsHitTesting(false)
+                Text("Satellite")
+                    .frame(width: 136, height: 14)
+                    .foregroundColor(!isMapSelected ? Color.black : Color.white)
+                    .allowsHitTesting(false)
+            }
+            
+        }
+    }
+}
+
+struct CapsuleBackground: View {
+    @Binding var isMapSelected: Bool
+    
+    var body: some View {
+        Capsule()
+            .fill(Color.white)
+            .frame(width: 136, height: 48)
+            .offset(x: isMapSelected ? -72 : 72)
+            .onChange(of: isMapSelected) { newValue, _ in
+                withAnimation(.easeInOut(duration: 0.15)) {}
+            }
+    }
+}
+
+//#Preview {
+//    MapControlButton()
+//}
--- VisionaryTour/Views/MapView/MapView.swift
+++ VisionaryTour/Views/MapView/MapView.swift
@@ -18,7 +18,7 @@ struct MapView: View {
                 .clipShape(RoundedRectangle(cornerRadius: 32))
                 .padding(EdgeInsets(top: 0, leading: 32, bottom: 32, trailing: 32))
                 .navigationBarItems(leading:
-                    ToggleButton(isMapSelected: $isMapSelected, viewModel: viewModel)
+                    MapControlButton(isMapSelected: $isMapSelected, viewModel: viewModel)
                         .padding(.leading, 8)
                 , trailing: HStack {
                     Button("map") {
@@ -30,89 +30,6 @@ struct MapView: View {
                 })
         }
         .searchable(text: $searchText)
-    }
-}
-
-struct ToggleButton: View {
-    @Binding var isMapSelected: Bool
-    let viewModel: MapViewModel
-    
-    var body: some View {
-        ZStack {
-            
-            Capsule()
-                .fill(Color(red:1.0, green: 1.0, blue: 1.0, opacity: 0.45))
-                .frame(width: 280, height: 48)
-            
-            HStack {
-                Button(action: {
-                    withAnimation {
-                        viewModel.updateMapType("roadmap")
-                        isMapSelected = true
-                    }
-                }) {
-                    Text("")
-                        .frame(width: 64, height: 14)
-                        .padding()
-                        .background(
-                            Capsule()
-                                .fill(Color.clear)
-                        )
-                        .contentShape(Capsule())
-                }
-                
-                Button(action: {
-                    withAnimation {
-                        viewModel.updateMapType("satellite")
-                        isMapSelected = false
-                    }
-                }) {
-                    Text("")
-                        .frame(width: 64, height: 14)
-                        .padding()
-                        .background(
-                            Capsule()
-                                .fill(Color.clear)
-                        )
-                        .contentShape(Capsule())
-                }
-            }
-            
-            Capsule()
-                .fill(Color.clear)
-                .innerShadow(color: Color.black.opacity(0.2), radius: 2)
-                .frame(width: 280, height: 48)
-                .allowsHitTesting(false)
-            
-            CapsuleBackground(isMapSelected: $isMapSelected)
-                .allowsHitTesting(false)
-            
-            HStack {
-                Text("Map")
-                    .frame(width: 136, height: 14)
-                    .foregroundColor(isMapSelected ? Color.black : Color.white)
-                    .allowsHitTesting(false)
-                Text("Satellite")
-                    .frame(width: 136, height: 14)
-                    .foregroundColor(!isMapSelected ? Color.black : Color.white)
-                    .allowsHitTesting(false)
-            }
-            
-        }
-    }
-}
-
-struct CapsuleBackground: View {
-    @Binding var isMapSelected: Bool
-    
-    var body: some View {
-        Capsule()
-            .fill(Color.white)
-            .frame(width: 136, height: 48)
-            .offset(x: isMapSelected ? -72 : 72)
-            .onChange(of: isMapSelected) { newValue, _ in
-                withAnimation(.easeInOut(duration: 0.15)) {}
-            }
     }
 }
 
