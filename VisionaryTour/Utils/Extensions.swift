ec1bbe51e5c721e2620f2fb613736566ce6b8c38
J-Naish <mjyrgd2332@i.softbank.jp>
2024-05-08 02:46:06 +0900

    move extensions

---
+++ VisionaryTour/Utils/Extensions.swift
@@ -0,0 +1,46 @@
+//
+//  Extensions.swift
+//  VisionaryTour
+//
+//  Created by JinNash on 2024/05/08.
+//
+
+import SwiftUI
+import Foundation
+
+extension View {
+    func innerShadow(color: Color, radius: CGFloat = 4, offsetX: CGFloat = 0, offsetY: CGFloat = 0) -> some View {
+        modifier(ShadowModifier(color: color, radius: radius, offsetX: offsetX, offsetY: offsetY))
+    }
+}
+
+struct ShadowModifier: ViewModifier {
+    let color: Color
+    let radius: CGFloat
+    let offsetX: CGFloat
+    let offsetY: CGFloat
+    
+    func body(content: Content) -> some View {
+        content
+            .overlay(
+                Capsule()
+                    .stroke(color, lineWidth: 4)
+                    .blur(radius: radius)
+                    .offset(x: offsetX, y: offsetY)
+                    .mask(
+                        Capsule()
+                            .fill(LinearGradient(gradient: Gradient(colors: [Color.black, Color.clear]), startPoint: .top, endPoint: .bottom))
+                    )
+            )
+            .overlay(
+                Capsule()
+                    .stroke(color, lineWidth: 4)
+                    .blur(radius: radius)
+                    .offset(x: offsetX, y: offsetY)
+                    .mask(
+                        Capsule()
+                            .fill(LinearGradient(gradient: Gradient(colors: [Color.clear, Color.black]), startPoint: .top, endPoint: .bottom))
+                    )
+            )
+    }
+}
--- VisionaryTour/Views/MapView/MapView.swift
+++ VisionaryTour/Views/MapView/MapView.swift
@@ -33,44 +33,6 @@ struct MapView: View {
     }
 }
 
-
-extension View {
-    func innerShadow(color: Color, radius: CGFloat = 4, offsetX: CGFloat = 0, offsetY: CGFloat = 0) -> some View {
-        modifier(ShadowModifier(color: color, radius: radius, offsetX: offsetX, offsetY: offsetY))
-    }
-}
-
-struct ShadowModifier: ViewModifier {
-    let color: Color
-    let radius: CGFloat
-    let offsetX: CGFloat
-    let offsetY: CGFloat
-    
-    func body(content: Content) -> some View {
-        content
-            .overlay(
-                Capsule()
-                    .stroke(color, lineWidth: 4)
-                    .blur(radius: radius)
-                    .offset(x: offsetX, y: offsetY)
-                    .mask(
-                        Capsule()
-                            .fill(LinearGradient(gradient: Gradient(colors: [Color.black, Color.clear]), startPoint: .top, endPoint: .bottom))
-                    )
-            )
-            .overlay(
-                Capsule()
-                    .stroke(color, lineWidth: 4)
-                    .blur(radius: radius)
-                    .offset(x: offsetX, y: offsetY)
-                    .mask(
-                        Capsule()
-                            .fill(LinearGradient(gradient: Gradient(colors: [Color.clear, Color.black]), startPoint: .top, endPoint: .bottom))
-                    )
-            )
-    }
-}
-
 #Preview() {
     MapView()
 }
