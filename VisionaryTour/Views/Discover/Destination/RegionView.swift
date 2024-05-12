85b80bc8578b2e068e5351edee9d76ccc41b0519
J-Naish <mjyrgd2332@i.softbank.jp>
2024-05-12 19:30:42 +0900

    update world heritage view

--- VisionaryTour/Views/Discover/Destination/RegionView.swift
+++ VisionaryTour/Views/Discover/Destination/RegionView.swift
@@ -10,7 +10,27 @@ import SwiftUI
 struct RegionView: View {
     var body: some View {
         NavigationStack {
-            RoundedSquareImage(image: Image("dummy"), size: .large, text: "Region")
+            ScrollView {
+                VStack(alignment: .leading) {
+                    RoundedSquareImage(image: Image("dummy"), size: .large, text: "Region")
+                        .padding(.bottom, 48)
+                    HStack(spacing: 32) {
+                        RoundedSquareImage(image: Image("dummy"), size: .medium, text: "Asia")
+                        RoundedSquareImage(image: Image("dummy"), size: .medium, text: "Europe")
+                    }
+                    .padding(.bottom, 32)
+                    HStack(spacing: 32) {
+                        RoundedSquareImage(image: Image("dummy"), size: .medium, text: "North America")
+                        RoundedSquareImage(image: Image("dummy"), size: .medium, text: "Latin America")
+                    }
+                    .padding(.bottom, 32)
+                    HStack(spacing: 32) {
+                        RoundedSquareImage(image: Image("dummy"), size: .medium, text: "Africa")
+                        RoundedSquareImage(image: Image("dummy"), size: .medium, text: "Oceania")
+                    }
+                    .padding(.bottom, 48)
+                }
+            }
         }
     }
 }
