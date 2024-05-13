b2fa53210c6aeaba0f8a53ed302239adacf1acee
J-Naish <mjyrgd2332@i.softbank.jp>
2024-05-13 21:35:01 +0900

    update the preview of region view

--- VisionaryTour/Views/Discover/Destination/RegionView.swift
+++ VisionaryTour/Views/Discover/Destination/RegionView.swift
@@ -8,31 +8,38 @@
 import SwiftUI
 
 struct RegionView: View {
+    @EnvironmentObject var modelData: ModelData
+    
     var body: some View {
-        
         ScrollView {
             VStack(alignment: .leading) {
                 RoundedSquareImage(image: Image("dummy"), size: .large, text: "Region")
                     .padding(.bottom, 72)
-                
-
                 HStack(spacing: 32) {
-                    RoundedSquareImage(image: Image("dummy"), size: .medium, text: "Asia")
-                    RoundedSquareImage(image: Image("dummy"), size: .medium, text: "Europe")
+                    NavigationLink(destination: RegionListView(region: .asia)) {
+                        RoundedSquareImage(image: Image("dummy"), size: .medium, text: "Asia")
+                    }
+                    NavigationLink(destination: RegionListView(region: .europe)) {
+                        RoundedSquareImage(image: Image("dummy"), size: .medium, text: "Europe")
+                    }
                 }
                 .padding(.bottom, 32)
-                
-
                 HStack(spacing: 32) {
-                    RoundedSquareImage(image: Image("dummy"), size: .medium, text: "North America")
-                    RoundedSquareImage(image: Image("dummy"), size: .medium, text: "Latin America")
+                    NavigationLink(destination: RegionListView(region: .northAmerica)) {
+                        RoundedSquareImage(image: Image("dummy"), size: .medium, text: "North America")
+                    }
+                    NavigationLink(destination: RegionListView(region: .latinAmerica)) {
+                        RoundedSquareImage(image: Image("dummy"), size: .medium, text: "Latin America")
+                    }
                 }
                 .padding(.bottom, 32)
-                
-
                 HStack(spacing: 32) {
-                    RoundedSquareImage(image: Image("dummy"), size: .medium, text: "Africa")
-                    RoundedSquareImage(image: Image("dummy"), size: .medium, text: "Oceania")
+                    NavigationLink(destination: RegionListView(region: .africa)) {
+                        RoundedSquareImage(image: Image("dummy"), size: .medium, text: "Africa")
+                    }
+                    NavigationLink(destination: RegionListView(region: .oceania)) {
+                        RoundedSquareImage(image: Image("dummy"), size: .medium, text: "Oceania")
+                    }
                 }
                 .padding(.bottom, 48)
             }
@@ -40,6 +47,9 @@ struct RegionView: View {
     }
 }
 
-#Preview {
-    RegionView()
+struct RegionView_Previews: PreviewProvider {
+    static var previews: some View {
+        RegionView()
+            .environmentObject(ModelData())
+    }
 }
