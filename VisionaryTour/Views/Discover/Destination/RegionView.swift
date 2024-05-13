71dcdf4e43153669462e508d273cbfeed7add266
J-Naish <mjyrgd2332@i.softbank.jp>
2024-05-13 21:27:24 +0900

    add empty line in region view

--- VisionaryTour/Views/Discover/Destination/RegionView.swift
+++ VisionaryTour/Views/Discover/Destination/RegionView.swift
@@ -14,16 +14,22 @@ struct RegionView: View {
             VStack(alignment: .leading) {
                 RoundedSquareImage(image: Image("dummy"), size: .large, text: "Region")
                     .padding(.bottom, 72)
+                
+
                 HStack(spacing: 32) {
                     RoundedSquareImage(image: Image("dummy"), size: .medium, text: "Asia")
                     RoundedSquareImage(image: Image("dummy"), size: .medium, text: "Europe")
                 }
                 .padding(.bottom, 32)
+                
+
                 HStack(spacing: 32) {
                     RoundedSquareImage(image: Image("dummy"), size: .medium, text: "North America")
                     RoundedSquareImage(image: Image("dummy"), size: .medium, text: "Latin America")
                 }
                 .padding(.bottom, 32)
+                
+
                 HStack(spacing: 32) {
                     RoundedSquareImage(image: Image("dummy"), size: .medium, text: "Africa")
                     RoundedSquareImage(image: Image("dummy"), size: .medium, text: "Oceania")
