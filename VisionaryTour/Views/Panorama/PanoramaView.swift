dc4360d24082905c141d9c41d2e87557bc0a1b49
J-Naish <mjyrgd2332@i.softbank.jp>
2024-05-15 01:00:15 +0900

    create basic of panorama view

---
+++ VisionaryTour/Views/Panorama/PanoramaView.swift
@@ -0,0 +1,27 @@
+//
+//  PanoramaView.swift
+//  VisionaryTour
+//
+//  Created by JinNash on 2024/05/15.
+//
+
+import SwiftUI
+import RealityKit
+
+struct PanoramaView: View {
+    
+    var viewModel: ImmersiveViewModel
+    
+    var body: some View {
+        RealityView { content in
+            content.add(viewModel.setupContentEnity())
+        }
+        .task {
+            try? await viewModel.setSnapshot()
+        }
+    }
+}
+
+#Preview {
+    PanoramaView(viewModel: ImmersiveViewModel())
+}
