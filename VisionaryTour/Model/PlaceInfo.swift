7a99bf37090261d3be6b94a6ce81236ea988f9cc
J-Naish <mjyrgd2332@i.softbank.jp>
2024-05-17 00:53:13 +0900

    update place info type

--- VisionaryTour/Model/PlaceInfo.swift
+++ VisionaryTour/Model/PlaceInfo.swift
@@ -7,7 +7,22 @@
 
 import MapKit
 
-struct PlaceInfo {
+struct PlaceInfo: Identifiable, Hashable {
+    let id: String
     var locationCoordinate: CLLocationCoordinate2D
     var panoId: String?
+    
+    init(locationCoordinate:CLLocationCoordinate2D, panoId: String?) {
+        self.locationCoordinate = locationCoordinate
+        self.panoId = panoId
+        self.id = panoId ?? UUID().uuidString
+    }
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+    }
+    
+    static func == (lhs: PlaceInfo, rhs: PlaceInfo) -> Bool {
+        lhs.id == rhs.id
+    }
 }
