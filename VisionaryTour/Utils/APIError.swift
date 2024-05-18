738a079c8e75a2eb82b201164ea966b328ef60aa
J-Naish <mjyrgd2332@i.softbank.jp>
2024-05-19 01:50:04 +0900

    revise api error type

--- VisionaryTour/Utils/APIError.swift
+++ VisionaryTour/Utils/APIError.swift
@@ -2,22 +2,22 @@
 //  APIError.swift
 //  VisionaryTour
 //
-//  Created by JinNash on 2024/05/16.
+//  Created by JinNash on 2024/05/19.
 //
 
 enum APIError: Error {
     case networkError
-    case unknown
-    case invalidResponse
-    
+    case invalidResponseError
+    case unknownError
+
     var title: String {
         switch self {
         case .networkError:
-            return "network error"
-        case .unknown:
-            return "unknown error"
-        case .invalidResponse:
+            return "netowrk error"
+        case .invalidResponseError:
             return "invalid response"
+        case .unknownError:
+            return "unknown error"
         }
     }
 }
