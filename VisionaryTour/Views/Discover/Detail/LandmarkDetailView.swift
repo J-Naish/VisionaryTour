9c4e1b009bcec2c0467517be579b086feda91551
J-Naish <mjyrgd2332@i.softbank.jp>
2024-05-23 15:57:04 +0900

    create basic of landmark detail view

---
+++ VisionaryTour/Views/Discover/Detail/LandmarkDetailView.swift
@@ -0,0 +1,47 @@
+//
+//  LandmarkDetailView.swift
+//  VisionaryTour
+//
+//  Created by JinNash on 2024/05/22.
+//
+
+import SwiftUI
+
+struct LandmarkDetailView: View {
+    var landmark: Landmark
+    
+    var body: some View {
+        HStack {
+            ScrollView {
+                VStack(alignment: .leading) {
+                    HStack {
+                        Image("dummy")
+                            .resizable()
+                            .frame(width: 120, height: 120)
+                            .cornerRadius(12)
+
+                        VStack(alignment: .leading) {
+                            Text(landmark.name)
+                                .font(.largeTitle)
+                            Text("\(landmark.state) / \(landmark.country.rawValue)")
+                        }
+                        .padding(.leading, 24)
+                    }
+                    .padding(.bottom, 32)
+                    
+                    Text("Description")
+                        .font(.title)
+                        .padding(.bottom, 16)
+                    
+                    Text(landmark.description)
+                }
+            }
+            
+            
+        }
+    }
+}
+
+#Preview {
+    LandmarkDetailView(landmark: ViewModel().landmarks.first!)
+}
---
+++ VisionaryTour/Views/Discover/Detail/landmark.js
@@ -0,0 +1,32 @@
+let map;
+let marker;
+
+function initMap() {
+
+    // initial position; where Apple HQ is
+    const center = { lat: 37.3346, lng: -122.0090 };
+
+    // create map
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: center,
+        zoom: 20,
+        fullscreenControl: false,
+        streetViewControl: false,
+        clickableIcons: false,
+        mapTypeControl: false,
+        zoomControl: false,
+        rotateControl: false
+    });
+}
+
+// load the map
+function loadScript() {
+    const script = document.createElement("script");
+    script.defer = true;
+    script.async = true;
+    document.head.appendChild(script);
+}
+
+
+// load the map when the window loads
+window.onload = loadScript;
