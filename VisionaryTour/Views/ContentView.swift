6d85ad8d3acd1bbad811c99eb22db1b035a787c7
J-Naish <mjyrgd2332@i.softbank.jp>
2024-06-16 17:51:12 +0900

    replace home icon

--- VisionaryTour/Views/ContentView.swift
+++ VisionaryTour/Views/ContentView.swift
@@ -31,7 +31,7 @@ struct ContentView: View {
         TabView(selection: $selection) {
             HomeView(viewModel: viewModel, immersiveViewModel: immersiveViewModel, showImmersiveSpace: $showImmersiveSpace)
                 .tabItem {
-                    Label("Discover", systemImage: "magnifyingglass")
+                    Label("Discover", systemImage: "house")
                 }
                 .tag(Tab.home)
             
