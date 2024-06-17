c3e6c95fef52a5f551e5846258491385446bf31d
J-Naish <mjyrgd2332@i.softbank.jp>
2024-06-17 19:29:53 +0900

    create basic of recommended view

---
+++ VisionaryTour/Views/Home/Destination/Discover/RecommendedView.swift
@@ -0,0 +1,24 @@
+//
+//  RecommendedView.swift
+//  VisionaryTour
+//
+//  Created by JinNash on 2024/06/17.
+//
+
+import SwiftUI
+
+struct RecommendedView: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            SectionTitleView(title: "Recommended")
+            
+            ScrollView(.horizontal) {
+                
+            }
+        }
+    }
+}
+
+#Preview {
+    RecommendedView()
+}
