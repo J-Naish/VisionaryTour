f8fb7b98258313934a1887a0d72992d192d082f8
J-Naish <mjyrgd2332@i.softbank.jp>
2024-06-18 18:45:05 +0900

    update definition of width of the list item

--- VisionaryTour/Views/Home/Destination/List/ListItemView.swift
+++ VisionaryTour/Views/Home/Destination/List/ListItemView.swift
@@ -46,9 +46,9 @@ struct ListItemView: View {
                     .background(.white)
                     .opacity(0.1)
                     .blur(radius: 4)
-                    .frame(width: 720, height: 120)
             }
         }
+        .frame(width: 800, height: 120)
         .overlay(
             CustomRoundedBorderShape(cornerRadius: 16, corners: [.topLeft, .bottomLeft, .topRight, .bottomRight])
                 .stroke(Color(red: 1, green: 1, blue: 1, opacity: 0.6), lineWidth: 0.5)
