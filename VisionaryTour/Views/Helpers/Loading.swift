8cfb0c8551235bffe2ce97af16f5d5504fd004c9
J-Naish <mjyrgd2332@i.softbank.jp>
2024-06-20 14:20:53 +0900

    update loading comp

--- VisionaryTour/Views/Helpers/Loading.swift
+++ VisionaryTour/Views/Helpers/Loading.swift
@@ -13,18 +13,25 @@ struct Loading: View {
     
     var body: some View {
         if immersiveViewModel.progress < 1.0 {
-            VStack {
-                ProgressView(value: immersiveViewModel.progress)
-                    .scaleEffect(2.0)
-                    .frame(width: 100, height: 100)
-                    .progressViewStyle(.circular)
-                Text("Loading Panorama")
-                    .font(.largeTitle)
+            ZStack {
+                Color(hex: "#808080", opacity: 0.3)
+                    .blur(radius: 20)
+                
+                VStack {
+                    ProgressView(value: immersiveViewModel.progress)
+                        .scaleEffect(2.0)
+                        .frame(width: 100, height: 100)
+                        .progressViewStyle(.circular)
+                    Text("Loading Panorama")
+                        .font(.largeTitle)
+                }
             }
+            .frame(width: 400, height: 240)
+            .clipShape(RoundedRectangle(cornerRadius: 24))
         }
     }
 }
 
-//#Preview {
-//    Loading()
-//}
+#Preview {
+    Loading(immersiveViewModel: ImmersiveViewModel(placeInfo: defaultPlace))
+}
